8614ddb90123c779f3aace262c5ee8856cb1b957
Jitu <jitu123435@gmail.com>
2024-06-30 00:44:17 +0530

    test  dependacy fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { act } from "react-dom/test-utils";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
+let container: HTMLDivElement | null = null;
+let root: ReturnType<typeof createRoot> | null = null;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+  if (container) {
+    root = createRoot(container);
+  }
+});
+
+afterEach(() => {
+  if (container) {
+    document.body.removeChild(container);
+    container = null;
+  }
+  root = null;
+});
+
+test("renders learn react link", () => {
+  act(() => {
+    if (root) {
+      root.render(<App />);
+    }
+  });
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
